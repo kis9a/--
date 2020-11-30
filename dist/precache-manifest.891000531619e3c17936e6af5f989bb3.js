409479df9c04109dbd77e8b344f35f10f4ac97fb
kis9a <kait0.min.w@gmail.com>
2020-11-30 16:40:51 +0900

    restructure architecture vue-cli create

---
+++ dist/precache-manifest.891000531619e3c17936e6af5f989bb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01503bf03605e6493fc7",
+    "url": "/css/app.41eceb5d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f1ea4e36b7e1267f2270a2abe657c7dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f04e9f7394309255699",
+    "url": "/js/about.71b95ce9.js"
+  },
+  {
+    "revision": "01503bf03605e6493fc7",
+    "url": "/js/app.0fa1e9e5.js"
+  },
+  {
+    "revision": "c4c356ca90c8d64075e2",
+    "url": "/js/chunk-vendors.cb64a4fc.js"
+  },
+  {
+    "revision": "248764d12875a71ef3ae4ea0a2d9070f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
