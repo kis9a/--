3ceaddc19581c344daf7d2713bccb91f9041b62a
kis9a <kait0.min.w@gmail.com>
2020-12-02 11:40:59 +0900

    clean: remove extra codes and set layotu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">KIS9A</router-link>
   </div>
   <router-view/>
+  <CFooter />
 </template>
+
+<script lang='ts'>
+import CFooter from '@/components/CFooter.vue'
+import { defineComponent } from 'vue'
+import 'normalize.css'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  CFooter
+}
+
+})
+</script>
 
 <style>
 #app {
@@ -13,18 +27,24 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 5% auto;
+  width: 76%;
+  max-width: 640px;
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
+  text-align: start;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: inherit;
+  text-decoration: none;
+  font-size: 15px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: black;
 }
 </style>
---
+++ src/components/CFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="c-footer-container">
+      <div class="links">
+        <a href="https://github.com/kis9a" target="_blank">Github</a>
+        <a href="https://medium.com/@kis9a" target="_blank">Medium</a>
+        <a href="https://qiita.com/kis9a" target="_blank">Qiita</a>
+        <a href="https://twitter.com/kis9a" target="_blank">Twitter</a>
+        <a href="https://instagram.com/kis9a" target="_blank">Instagram</a>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CFooter',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.links {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  padding: 15px;
+}
+a {
+padding: 4px;
+font-size: 12px;
+  color: gray;
+}
+</style>
