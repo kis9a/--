0072a542812a4e8bebfbb5495d147bfa0917a26d
kis9a <kis9ax@gmail.com>
2020-12-18 06:56:47 +0900

    feat: App.vue animation move to Home component

--- src/App.vue
+++ src/App.vue
@@ -2,73 +2,19 @@
   <div id="nav">
     <router-link to="/">KIS9A</router-link>
   </div>
-  <div id="canvas"></div>
   <router-view />
   <CFooter />
 </template>
 
 <script lang="js">
-import CFooter from "@/components/CFooter.vue";
 import { defineComponent } from "vue";
-import '@pixi/filter-displacement'
-import * as PIXI from 'pixi.js'
-import kis9aImg from '@/assets/kis9ab.png'
-import filterImg from '@/assets/filter.jpg'
+import CFooter from "@/components/CFooter.vue";
 import "normalize.css";
 
 export default defineComponent({
   name: "App",
   components: {
     CFooter,
-  },
-  mounted() {
-    const pixi = new PIXI.Application({
-      width: 300,
-      height: 240,
-      transparent: true,
-    });
-    document.querySelector('#canvas').append(pixi.view);
-
-    pixi.renderer.plugins.interaction.moveWhenInside = !0,
-    pixi.renderer.backgroundColor = '#f00';
-    pixi.stage.interactive = !0;
-
-    const pixiContainer = new PIXI.Container();
-    pixi.stage.addChild(pixiContainer);
-
-    const kis9a = PIXI.Sprite.from(kis9aImg);
-    pixiContainer.addChild(kis9a);
-    kis9a.x = 50;
-    kis9a.y = -42;
-    kis9a.scale.set(0.6)
-    kis9a.width = 300;
-    kis9a.heigh = 300;
-
-    const filter = PIXI.Sprite.from(filterImg);
-    filter.scale.set(1);
-    filter.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-
-    const graphics= new PIXI.Graphics();
-    graphics.interactive = !0;
-    graphics.button = !1;
-    pixi.stage.addChild(graphics);
-    graphics.beginFill(0, 0.0001);
-    graphics.drawRect(10, 10, 680, 580);
-    graphics.endFill();
-
-    const displacementFilter = new PIXI.filters.DisplacementFilter(filter);
-    displacementFilter.padding = 20;
-    pixi.stage.addChild(filter);
-    kis9a.filters = [displacementFilter];
-    displacementFilter.scale.x = 30;
-    displacementFilter.scale.y = 50;
-
-    pixi.ticker.add(() => {
-        filter.x += 4;
-        if (filter.x > filter.width) {
-          { filter.x = 0; }
-        }
-    });
   }
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,68 @@
 <template>
-  <div class="home">
-  </div>
+  <div id="canvas"></div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import { defineComponent } from "vue";
+import '@pixi/filter-displacement'
+import * as PIXI from 'pixi.js'
+import kis9aImg from '@/assets/kis9ab.png'
+import filterImg from '@/assets/filter.jpg'
 
     /* <img alt="kis9a" class="hero" src="@/assets/kis9a.jpg"> */
 export default defineComponent({
   name: "Home",
+  component: {},
+  mounted() {
+    PIXI.utils.skipHello();
+    const pixi = new PIXI.Application({
+      width: 300,
+      height: 240,
+      transparent: true,
+    });
+    document.querySelector('#canvas').append(pixi.view);
+
+    pixi.renderer.plugins.interaction.moveWhenInside = !0,
+    pixi.renderer.backgroundColor = '#f00';
+    pixi.stage.interactive = !0;
+
+    const pixiContainer = new PIXI.Container();
+    pixi.stage.addChild(pixiContainer);
+
+    const kis9a = PIXI.Sprite.from(kis9aImg);
+    pixiContainer.addChild(kis9a);
+    kis9a.x = 50;
+    kis9a.y = -42;
+    kis9a.scale.set(0.6)
+    kis9a.width = 300;
+    kis9a.heigh = 300;
+
+    const filter = PIXI.Sprite.from(filterImg);
+    filter.scale.set(1);
+    filter.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+
+    const graphics= new PIXI.Graphics();
+    graphics.interactive = !0;
+    graphics.button = !1;
+    pixi.stage.addChild(graphics);
+    graphics.beginFill(0, 0.0001);
+    graphics.drawRect(10, 10, 680, 580);
+    graphics.endFill();
+
+    const displacementFilter = new PIXI.filters.DisplacementFilter(filter);
+    displacementFilter.padding = 20;
+    pixi.stage.addChild(filter);
+    kis9a.filters = [displacementFilter];
+    displacementFilter.scale.x = 30;
+    displacementFilter.scale.y = 50;
+
+    pixi.ticker.add(() => {
+        filter.x += 4;
+        if (filter.x > filter.width) {
+          { filter.x = 0; }
+        }
+    });
+  }
 });
 </script>
 
