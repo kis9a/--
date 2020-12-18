9c41d9129d0f3879fe8533f9199a8e4a83cceb3f
kis9a <kis9ax@gmail.com>
2020-12-18 20:00:25 +0900

    feat: compression .jpg .png to webp, imagemin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,37 @@
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const ImageminWebp = require("imagemin-webp-webpack-plugin");
+const ImageminMozJpeg = require("imagemin-mozjpeg");
+
 module.exports = {
-    pwa: {
-        workboxOptions: {
-            skipWaiting: true
-        }
-    }
-}
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
+  chainWebpack(config) {
+    config.plugin("ImageminWebp").use(ImageminWebp, [
+      {
+        test: /\.(jpg|png)$/i,
+        option: {
+          quality: 95,
+        },
+      },
+    ]);
+
+    config.plugin("ImageminPlugin").use(ImageminPlugin, [
+      {
+        test: /\.(jpg|png)$/i,
+        disable: process.env.NODE_ENV !== "production",
+        pngquant: {
+          quality: "95",
+        },
+        plugins: [
+          ImageminMozJpeg({
+            quality: 95,
+            progressive: true,
+          }),
+        ],
+      },
+    ]);
+  },
+};
