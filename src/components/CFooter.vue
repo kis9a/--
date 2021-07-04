a2e5dc35b5fb7d73e36eaac5039bd0c26b1fe694
kis9a <kis9ax@gmail.com>
2021-07-05 00:33:17 +0900

    feat: add zenn icon and add link me.kis9a.com

--- src/components/CFooter.vue
+++ src/components/CFooter.vue
@@ -11,20 +11,40 @@
           <TwitterIcon />
         </a>
         <a
-          href="https://zenn.dev/kis9a"
-          target="_blank"
-          aria-label="document"
-          rel="noopener"
-        >
-          <DocumentIcon />
-        </a>
-        <a
           href="https://github.com/kis9a"
           target="_blank"
           aria-label="github"
           rel="noopener"
         >
           <GithubIcon />
+        </a>
+        <a
+          href="https://zenn.dev/kis9a"
+          target="_blank"
+          aria-label="zenn"
+          rel="noopener"
+        >
+          <svg
+            width="1em"
+            height="1em"
+            class="ion zenn-svg"
+            role="img"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Zenn</title>
+            <path
+              d="M.264 23.771h4.984c.264 0 .498-.147.645-.352L19.614.874c.176-.293-.029-.645-.381-.645h-4.72c-.235 0-.44.117-.557.323L.03 23.361c-.088.176.029.41.234.41zM17.445 23.419l6.479-10.408c.205-.323-.029-.733-.41-.733h-4.691c-.176 0-.352.088-.44.235l-6.655 10.643c-.176.264.029.616.352.616h4.779c.234-.001.468-.118.586-.353z"
+            />
+          </svg>
+        </a>
+        <a
+          href="https://me.kis9a.com"
+          target="_blank"
+          aria-label="document"
+          rel="noopener"
+        >
+          <DocumentIcon />
         </a>
       </div>
     </div>
@@ -62,10 +82,10 @@ export default defineComponent({
 
 a {
   padding: 4px 5px;
+  margin: auto 0;
 }
 
 .ion {
-  font-size: 13px;
   color: black;
 }
 
